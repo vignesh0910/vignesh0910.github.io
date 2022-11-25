6442f42e7e41a2b29634b19d226dc2fe955293c3
Vignesh B <78461372+vignesh0910@users.noreply.github.com>
2022-11-25 05:48:30 +0530

    Update script.js

--- Module-4-main/script.js
+++ Module-4-main/script.js
@@ -28,19 +28,24 @@ WARNING!!! WARNING!!!
 */
 
 
-(function () {
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
 }
-
-})();
